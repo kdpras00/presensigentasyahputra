86c18b77e43735f83fba505b919f3cf07184ed88
kdpras00 <kdpras00@gmail.com>
2026-06-15 07:05:10 +0700

    feat(database): add ScheduleSeeder to DatabaseSeeder class
    - Included ScheduleSeeder in the DatabaseSeeder seeder array
    - Enabled seeding of schedule data during database seeding process

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
             TeacherSeeder::class,
             StudentSeeder::class,
+            ScheduleSeeder::class,
         ]);
     }
 }
